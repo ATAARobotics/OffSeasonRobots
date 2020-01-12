021ca5b695248e8794f48033b4c344dcab5a202c
andrew <randrewcrafter@outlook.com>
2020-01-12 15:37:28 -0700

    added acceleration for speed and turn

--- src/main/java/frc/robot/teleop/Teleop.java
+++ src/main/java/frc/robot/teleop/Teleop.java
@@ -17,9 +17,24 @@ public class Teleop {
         drive.init();
     }
 
+    private double previousSpeed = 0;
+    private double previousTurn = 0;
+
     public void teleopCyc() {
-        double speed = controller.getY(Hand.kLeft);
-        double turn = controller.getX(Hand.kRight);
+        double speed;
+        if (Math.abs(controller.getY(Hand.kLeft)) < 0.05) {
+            speed = 0;
+        } else {
+            speed = controller.getY(Hand.kLeft) * 0.1 + previousSpeed * 0.9;
+        }
+        previousSpeed = speed;
+        double turn;
+        if (Math.abs(controller.getX(Hand.kRight)) < 0.05) {
+            turn = 0;
+        } else {
+            turn = controller.getX(Hand.kRight) * 0.1 + previousTurn * 0.9;
+        }
+        previousTurn = turn;
         drive.drive(speed, turn, true); 
 
         if (controller.getStickButtonPressed(Hand.kRight)) {
